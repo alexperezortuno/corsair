2e063822115d8bf62fac1c5862d494fcca14033e
alexperezortuno <alexperezortuno@gmail.com>
2026-07-30 17:26:09 -0400

    Fix linting issues and update permissions in `wxt.config.ts`

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,28 +1,29 @@
-import { defineConfig } from 'wxt';
+import {defineConfig} from 'wxt';
 
 export default defineConfig({
-  modules: ['@wxt-dev/module-vue'],
+    modules: ['@wxt-dev/module-vue'],
 
-  manifest: {
-    name: 'Corsair HTTP Interceptor',
-    description:
-        'Intercepta, inspecciona y modifica solicitudes y respuestas HTTP.',
-    version: '0.1.0',
+    manifest: {
+        name: 'Corsair HTTP Interceptor',
+        description:
+            'Intercept, inspect, and modify HTTP requests and responses.',
+        version: '0.1.0',
 
-    minimum_chrome_version: '116',
+        minimum_chrome_version: '116',
 
-    permissions: [
-      'debugger',
-      'storage',
-      'tabs',
-      'activeTab',
-      'sidePanel',
-    ],
-
-    host_permissions: ['<all_urls>'],
-
-    action: {
-      default_title: 'Abrir Corsair HTTP Interceptor',
+        permissions: [
+            'debugger',
+            'storage',
+            'tabs',
+            'activeTab',
+            'sidePanel',
+        ],
+        host_permissions: [
+            "https://*/**",
+            "<all_urls>"
+        ],
+        action: {
+            default_title: 'Open Corsair HTTP Interceptor',
+        },
     },
-  },
 });
