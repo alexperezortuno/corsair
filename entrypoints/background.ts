7bb6dc5f6e18f0fab0c9926056c5ca3f93bb862e
alexperezortuno <alexperezortuno@gmail.com>
2026-07-29 20:37:08 -0400

    Refactor background service worker to log initialization and add side panel functionality.

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,26 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  console.info('[Corsair] Background service worker iniciado');
+
+  chrome.runtime.onInstalled.addListener(() => {
+    console.info('[Corsair] Extensión instalada o actualizada');
+  });
+
+  chrome.action.onClicked.addListener(async (tab) => {
+    if (!tab.id) {
+      console.warn('[Corsair] No se encontró una pestaña válida');
+      return;
+    }
+
+    try {
+      await chrome.sidePanel.open({
+        tabId: tab.id,
+      });
+
+      console.info('[Corsair] Side Panel abierto', {
+        tabId: tab.id,
+      });
+    } catch (error) {
+      console.error('[Corsair] No fue posible abrir el Side Panel', error);
+    }
+  });
 });
